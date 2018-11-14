815f34b4c3e09d91a082c6372aa4b655583425bc
mihdan <mikhail@kobzarev.com>
2018-11-14 21:36:08 +0300

    Удалил лишние кнопки в likely

--- wp-content/themes/twentynineteen-child/template-parts/likely.php
+++ wp-content/themes/twentynineteen-child/template-parts/likely.php
@@ -1,11 +1,10 @@
 <div class="likely">
-	<div class="facebook">Share</div>
-	<div class="twitter">Tweet</div>
-	<div class="gplus">+1</div>
-	<div class="vkontakte">Share</div>
-	<div class="pinterest">Pin</div>
-	<div class="odnoklassniki">Like</div>
-	<div class="telegram">Send</div>
-	<div class="linkedin">Share</div>
-	<div class="whatsapp">Send</div>
+	<div class="facebook">Лайкнуть</div>
+	<div class="twitter">Твитнуть</div>
+	<div class="vkontakte">Пошерить</div>
+	<div class="pinterest">Запинить</div>
+	<?php if ( wp_is_mobile() ) : ?>
+		<div class="telegram">В телегу</div>
+		<div class="whatsapp">В вотсап</div>
+	<?php endif; ?>
 </div>
