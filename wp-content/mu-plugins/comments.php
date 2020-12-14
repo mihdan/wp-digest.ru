3ba419d9250348361198fd6ef9ff0bb927da2974
mihdan <mikhail@kobzarev.com>
2020-12-14 17:46:58 +0300

    Add comments link to post header

--- wp-content/mu-plugins/comments.php
+++ wp-content/mu-plugins/comments.php
@@ -38,3 +38,15 @@ add_action(
 	},
 	2
 );
+
+add_action(
+	'kadence_after_entry_meta',
+	function () {
+	    if ( ! is_singular( [ 'post', 'vacancy', 'resume' ] ) ) {
+	        return;
+        }
+		?>
+        <span><svg style="margin-right: 5px" width="1.1em" height="1.1em" viewBox="0 0 32 32"><path fill="#2d3748" d="M28.8 9.6v11.2c0 1.76-1.44 3.2-3.2 3.2h-6.4v4.8l-6.4-4.8h-6.4c-1.762 0-3.2-1.44-3.2-3.2v-11.2c0-1.76 1.438-3.2 3.2-3.2h19.2c1.76 0 3.2 1.44 3.2 3.2z"></path></svg><a href="#reply-title">Комментарии</a></span>
+		<?php
+	}
+);
