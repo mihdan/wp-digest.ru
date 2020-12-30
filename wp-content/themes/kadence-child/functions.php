457247ececaab1464714f08d5ba80694fab6ae78
mihdan <mikhail@kobzarev.com>
2020-12-30 17:15:14 +0300

    Добавил DIC в проект

--- wp-content/themes/kadence-child/functions.php
+++ wp-content/themes/kadence-child/functions.php
@@ -1,1 +1,16 @@
 <?php
+namespace Mihdan\Kadence_Child;
+
+use Auryn\Injector;
+
+// @codeCoverageIgnoreStart
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+// @codeCoverageIgnoreEnd
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+
+	( new Main( new Injector() ) )->init();
+}
