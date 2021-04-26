9d93318531ca7541b173154e59b8af7077e63047
mihdan <mikhail@kobzarev.com>
2021-04-26 14:28:18 +0300

    Отключает шрифты в теме и добавляет прелоад для фото записи

--- wp-content/themes/kadence-child/src/class-performance.php
+++ wp-content/themes/kadence-child/src/class-performance.php
@@ -7,6 +7,7 @@ class Performance {
 			'wp_print_styles',
 			function () {
 				wp_deregister_style( 'kadence-comments' );
+				wp_deregister_style( 'kadence-fonts' );
 			}
 		);
 
@@ -15,6 +16,21 @@ class Performance {
 			function () {
 				wp_deregister_script( 'comment-reply' );
 				wp_deregister_script( 'wp-embed' );
+			}
+		);
+
+		add_action(
+			'wp_head',
+			function () {
+				if ( is_singular( 'post' ) && has_post_thumbnail() ) {
+					$thumbnail_id = get_post_thumbnail_id();
+					printf(
+						'<link rel="preload" as="image" imagesrcset="%s" imagesizes="%s" />',
+						wp_get_attachment_image_srcset( $thumbnail_id ),
+						wp_get_attachment_image_sizes( $thumbnail_id )
+					);
+					print( '<link rel="preconnect" href="https://newrrb.bid" />' );
+				}
 			}
 		);
 	}
